95d6539181363c8a2d4ae24e6fd9dc1df48f74f7
Gabriel Ciuloaica <gciuloaica@gmail.com>
2025-10-08 18:58:57 +0300

    [Sprint 1][Task 6] added tests

--- auth-service/tests/api/routes.rs
+++ auth-service/tests/api/routes.rs
@@ -1,1 +1,57 @@
+use crate::helpers::TestApp;
 
+// Tokio's test macro is used to run the test in an async environment
+#[tokio::test]
+async fn root_returns_auth_ui() {
+    let app = TestApp::new().await;
+
+    let response = app.get_root().await;
+
+    assert_eq!(response.status().as_u16(), 200);
+    assert_eq!(response.headers().get("content-type").unwrap(), "text/html");
+}
+
+#[tokio::test]
+async fn signup_returns_200() {
+    let app = TestApp::new().await;
+
+    let response = app.signup("testuser", "password").await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn login_returns_200() {
+    let app = TestApp::new().await;
+
+    let response = app.login("testuser", "password").await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn logout_returns_200() {
+    let app = TestApp::new().await;
+
+    let response = app.logout().await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn verify_2fa_returns_200() {
+    let app = TestApp::new().await;
+
+    let response = app.verify_2fa("2fa_token").await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn verify_token_returns_200() {
+    let app = TestApp::new().await;
+
+    let response = app.verify_token("jwt_token").await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
