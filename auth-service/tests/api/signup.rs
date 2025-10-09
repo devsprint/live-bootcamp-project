6fd27a7a38586ea7fe636c5ecf25ba5c34a1fc54
Gabriel Ciuloaica <gciuloaica@gmail.com>
2025-10-09 14:08:12 +0300

    [Sprint 1][Task 8] sign-up route basic implementation

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,10 +1,44 @@
-use crate::helpers::TestApp;
+use crate::helpers::{get_random_email, TestApp};
 
 #[tokio::test]
-async fn signup_returns_200() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
 
-    let response = app.signup("testuser", "password").await;
+    let random_email = get_random_email();
 
-    assert_eq!(response.status().as_u16(), 200);
+    let test_cases = [
+        serde_json::json!({
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "password": "",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "password": "password123",
+            "email": random_email,
+        }),
+        serde_json::json!({
+            "password": "password123",
+            "email": random_email,
+            "requires2FA": "not_a_boolean"
+        }),
+        serde_json::json!({
+            "password": "password123",
+            "email": random_email,
+            "requires2FA": null
+        }),
+        serde_json::json!({}),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await; // call `post_signup`
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
 }
